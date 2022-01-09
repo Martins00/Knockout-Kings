1ee0c7734ffc4334aca677992f33b6bc71bcd5dd
Martins Olatunji <Martinsolatunji2@gmail.com>
2022-01-09 18:44:51 +0100

    almost done, still working onthe responsiveness

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+// const hamburger = document.querySelector(".hamburger");
+// const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click", mobileMenu);
+// hamburger.addEventListener("click", mobileMenu);
 
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+// function mobileMenu() {
+//     hamburger.classList.toggle("active");
+//     navMenu.classList.toggle("active");
+// }
+
+// const navLink = document.querySelectorAll(".nav-link");
+
+// navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+// function closeMenu() {
+//     hamburger.classList.remove("active");
+//     navMenu.classList.remove("active");
+// }
+
+const box = (aboutUs) =>{
+    const box = document.getElementById("box");
+    const boxm = document.getElementById("boxm");
+
+    boxm.innerHTML = (aboutUs === undefined)? "" : aboutUs ;
+    box.style.display = (aboutUs === undefined)? "none" : "block";
 }
 
-const navLink = document.querySelectorAll(".nav-link");
+const container = (contactus) => {
+    const container = document.getElementById("container");
+    const containerm = document.getElementById("containerm");
 
-navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+    containerm.innerHTML = (contactus === undefined)? "" : contactus ;
+    container.style.display = (contactus === undefined)? "none" : "block";
 }
